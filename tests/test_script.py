9b3af8715337570a81d573487f7c078008fec362
sai-sujan <sujanofficial557@gmail.com>
2024-04-28 00:26:15 -0500

    changed tests

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,21 +1,28 @@
 import unittest
-
+import json
 from main import app
-import os
 
-
-class TestToPerform(unittest.TestCase):
+class TestFlaskApi(unittest.TestCase):
     def setUp(self):
+        # creates a test client
         self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
 
-    def tearDown(self):
-        pass
+    def test_random_recipe_route(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/recipe/random')
 
-    def test_page(self):
-        response = self.app.get('/', follow_redirects=True)
-        print(response)
-        self.assertEqual(response.status_code, 200)
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
 
+        # assert that the content returned by the route is JSON
+        self.assertEqual(result.content_type, 'application/json')
+
+        # if necessary, you can add more checks to validate the content of the response
+        # data = json.loads(result.get_data(as_text=True))
+        # self.assertIn('key', data)  # replace 'key' with actual key you expect in the response
 
 if __name__ == '__main__':
     unittest.main()
